67b2d2912347cfff3ae0f4c8704f868b0b725178
Tea <tea@null.net>
2024-07-30 17:00:53 +0100

    removed chromedriver & realised daft is forbidden... !o.o:

--- daftieScraper.go
+++ daftieScraper.go
@@ -6,18 +6,30 @@ import (
 	"github.com/gocolly/colly"
 )
 
+// order of callback calls :=
+// 1. OnRequest(): Called before performing an HTTP request with Visit().
+// 2. OnError(): Called if an error occurred during the HTTP request.
+// 3. OnResponse(): Called after receiving a response from the server.
+// 4. OnHTML(): Called right after OnResponse() if the received content is HTML.
+// 5. OnScraped(): Called after all OnHTML() callback executions are completed.
+
 func main() {
 
-	scrapeUrl := "https://www.daft.ie"
+	scrapeUrl := "https://go-colly.org/docs/introduction/configuration/"
 
-	clt := colly.NewCollector(colly.AllowedDomains("www.daft.ie", "daft.ie"))
+	clt := colly.NewCollector(colly.AllowedDomains("www.daft.ie", "daft.ie", "google.com", "go-colly.org"))
 
-	clt.OnHTML("h1.homepage-tagline", func(header *colly.HTMLElement) {
-		fmt.Println(header.Text)
+	// clt.OnHTML("h2.collector-configuration", func(h *colly.HTMLElement) {
+	// 	fmt.Println(h.Text)
+	// })
+
+	clt.OnHTML(".language-go", func(e *colly.HTMLElement) {
+		//fmt.Println(e)
+		fmt.Println(e.ChildText("span"))
 	})
 
 	clt.OnRequest(func(req *colly.Request) {
-		fmt.Printf(fmt.Sprintf("Visiting %s\n", req.URL))
+		fmt.Println("Visiting: ", req.URL)
 	})
 
 	clt.OnError(func(req *colly.Response, err error) {
@@ -25,5 +37,7 @@ func main() {
 	})
 
 	clt.Visit(scrapeUrl)
-	fmt.Println(222)
+
+	fmt.Println()
+
 }
