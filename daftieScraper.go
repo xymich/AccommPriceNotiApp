a55061f6d4bccd18aecaa6c3f1581a0f31e3f6a1
Tea <tea@null.net>
2024-08-12 17:16:40 +0100

    changed to chromepd for scraping

--- daftieScraper.go
+++ daftieScraper.go
@@ -2,42 +2,31 @@ package main
 
 import (
 	"fmt"
+	"log"
 
-	"github.com/gocolly/colly"
+	"github.com/playwright-community/playwright-go"
 )
 
-// order of callback calls :=
-// 1. OnRequest(): Called before performing an HTTP request with Visit().
-// 2. OnError(): Called if an error occurred during the HTTP request.
-// 3. OnResponse(): Called after receiving a response from the server.
-// 4. OnHTML(): Called right after OnResponse() if the received content is HTML.
-// 5. OnScraped(): Called after all OnHTML() callback executions are completed.
-
 func main() {
-
 	scrapeUrl := "https://go-colly.org/docs/introduction/configuration/"
 
-	clt := colly.NewCollector(colly.AllowedDomains("www.daft.ie", "daft.ie", "google.com", "go-colly.org"))
+	pw, err := playwright.Run()
+	if err != nil {
+		log.Fatalf("could not start playwright: %v", err)
+	}
+	browser, err := pw.Chromium.Launch()
+	if err != nil {
+		log.Fatalf("could not launch browser: %v", err)
+	}
+	page, err := browser.NewPage()
+	if err != nil {
+		log.Fatalf("could not create page: %v", err)
+	}
 
-	// clt.OnHTML("h2.collector-configuration", func(h *colly.HTMLElement) {
-	// 	fmt.Println(h.Text)
-	// })
+	if _, err = page.Goto(scrapeUrl); err != nil {
+		log.Fatalf("could not goto: %v", err)
+	}
 
-	clt.OnHTML(".language-go", func(e *colly.HTMLElement) {
-		//fmt.Println(e)
-		fmt.Println(e.ChildText("span"))
-	})
-
-	clt.OnRequest(func(req *colly.Request) {
-		fmt.Println("Visiting: ", req.URL)
-	})
-
-	clt.OnError(func(req *colly.Response, err error) {
-		fmt.Printf("Error while scraping: %s\n", err.Error())
-	})
-
-	clt.Visit(scrapeUrl)
-
-	fmt.Println()
+	fmt.Println(scrapeUrl)
 
 }
