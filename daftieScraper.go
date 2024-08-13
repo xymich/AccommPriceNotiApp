9858832da35d60d23acbf31fc1880d3cab2c973d
Tea <tea@null.net>
2024-08-13 11:05:52 +0100

    removed chromedp, now using playwright (more support for bypass)

--- daftieScraper.go
+++ daftieScraper.go
@@ -3,10 +3,46 @@ package main
 import (
 	"log"
 
-	"AccomPriceNotiApp/services"
-
 	"github.com/playwright-community/playwright-go"
 )
+
+var PlaywrightContext playwright.BrowserContext
+
+func InitializePlaywright() playwright.BrowserContext {
+	// Installation of browser and OS dependencies
+	err := playwright.Install()
+	if err != nil {
+		log.Fatalf("could not install playwright: %v", err)
+	}
+
+	// Running playwright
+	pw, err := playwright.Run(
+		&playwright.RunOptions{},
+	)
+	if err != nil {
+		log.Fatalf("could not start playwright: %v", err)
+	}
+
+	// Selecting Chromium as the browser
+	browser, err := pw.Chromium.Launch(playwright.BrowserTypeLaunchOptions{
+		Args: []string{"--disable-blink-features=AutomationControlled"},
+	})
+	if err != nil {
+		log.Fatalf("could not launch browser: %v", err)
+	}
+
+	// Creating context out of the created browser
+	context, err := browser.NewContext(playwright.BrowserNewContextOptions{
+		UserAgent: playwright.String("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"), // Fake user agent
+		Viewport:  &playwright.Size{Width: 1920, Height: 1080},
+	})
+
+	if err != nil {
+		log.Fatalf("could not create context: %v", err)
+	}
+
+	return context
+}
 
 func main() {
 	err := playwright.Install()
@@ -14,5 +50,6 @@ func main() {
 		log.Fatalf("could not install playwright: %v", err)
 	}
 
-	services.PlaywrightContext = services.InitializePlaywright()
+	PlaywrightContext = InitializePlaywright()
+
 }
--- go.mod
+++ go.mod
@@ -2,16 +2,12 @@ module AccomPriceNotiApp
 
 go 1.22.5
 
+require github.com/playwright-community/playwright-go v0.4501.1
+
 require (
 	github.com/deckarep/golang-set/v2 v2.6.0 // indirect
 	github.com/go-jose/go-jose/v3 v3.0.3 // indirect
 	github.com/go-stack/stack v1.8.1 // indirect
-	github.com/playwright-community/playwright-go v0.4501.1 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/exp v0.0.0-20240808152545-0cdaa3abc0fa // indirect
-	golang.org/x/net v0.17.0 // indirect
-	golang.org/x/sys v0.24.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
-	google.golang.org/appengine v1.6.6 // indirect
-	google.golang.org/protobuf v1.24.0 // indirect
 )
