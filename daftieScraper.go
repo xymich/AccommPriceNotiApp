981299d626ddbac400f6f0f84bd2c0ec05b0d892
Tea <tea@null.net>
2024-08-13 10:51:38 +0100

    changed to chromedp

--- daftieScraper.go
+++ daftieScraper.go
@@ -1,32 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"log"
+
+	"AccomPriceNotiApp/services"
 
 	"github.com/playwright-community/playwright-go"
 )
 
 func main() {
-	scrapeUrl := "https://go-colly.org/docs/introduction/configuration/"
-
-	pw, err := playwright.Run()
+	err := playwright.Install()
 	if err != nil {
-		log.Fatalf("could not start playwright: %v", err)
-	}
-	browser, err := pw.Chromium.Launch()
-	if err != nil {
-		log.Fatalf("could not launch browser: %v", err)
-	}
-	page, err := browser.NewPage()
-	if err != nil {
-		log.Fatalf("could not create page: %v", err)
+		log.Fatalf("could not install playwright: %v", err)
 	}
 
-	if _, err = page.Goto(scrapeUrl); err != nil {
-		log.Fatalf("could not goto: %v", err)
-	}
-
-	fmt.Println(scrapeUrl)
-
+	services.PlaywrightContext = services.InitializePlaywright()
 }
